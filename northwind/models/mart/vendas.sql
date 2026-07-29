334720acd4d49fc88c4e245255c444edc6556f2c
Michael <michael.mjpr@gmail.com>
2026-07-29 06:35:12 -0300

    Update vendas.sql
    test macro using updated_at column

--- northwind/models/mart/vendas.sql
+++ northwind/models/mart/vendas.sql
@@ -1,5 +1,17 @@
+{{config(
+    materialized='incremental',
+    unique_key='id'
+    ) }}
+
 with vendas as (
     select * from {{ref("stg_crm__nova_tabela")}}
 )
 
 select * from vendas
+
+{% if is_incremental() %}
+
+-- apenas registros novos
+where updated_at > (select max(updated_at) from {{this}})
+
+{% endif %}
