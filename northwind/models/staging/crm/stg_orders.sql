76af6e03adca6ee53f13152b6c13567383ab30d6
Michael <michael.mjpr@gmail.com>
2026-08-03 06:53:36 -0300

    Update stg_orders.sql
    Casting columns

--- northwind/models/staging/crm/stg_orders.sql
+++ northwind/models/staging/crm/stg_orders.sql
@@ -4,4 +4,19 @@ with sources as (
 
 )
 
-select * from sources
+select 
+    CAST(order_id AS VARCHAR) as order_id,
+    CAST(customer_id AS VARCHAR) as customer_id,
+    CAST(employee_id AS VARCHAR) as employee_id,
+    CAST(order_date AS DATE) as order_date,
+    CAST(required_date AS DATE) as required_date,
+    CAST(shipped_date AS DATE) as shipped_date,
+    CAST(ship_via AS VARCHAR) as ship_via,
+    CAST(freight AS DECIMAL(10,2)) as freight,
+    CAST(ship_name AS VARCHAR) as ship_name,
+    CAST(ship_address AS VARCHAR) as ship_address,
+    CAST(ship_city AS VARCHAR) as ship_city,
+    CAST(ship_region AS VARCHAR) as ship_region,
+    CAST(ship_postal_code AS VARCHAR) as ship_postal_code,
+    CAST(ship_country AS VARCHAR) as ship_country
+from sources
